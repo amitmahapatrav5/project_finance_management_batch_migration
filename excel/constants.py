3891ad884f8421bfbdf92486c75ac2fd39079100
Amit Kumar Mahapatra <36933827+AmitKumarMahapatra@users.noreply.github.com>
2024-07-28 15:55:50 +0530

    Backend Started

--- excel/constants.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-from pathlib import Path
-
-class Constants(Enum):
-    FILE_PATH = "./data/expense.xlsx"
-    WORKBOOK_NAME = "expense"
-    EXPENSE_SHEET_NAME = "expense"
-    CATEGORY_SHEET_NAME = "category"
-    SUB_CATEGORY_SHEET_NAME = "sub_category"
